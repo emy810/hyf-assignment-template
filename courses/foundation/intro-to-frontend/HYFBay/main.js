ecc9515283704066631e08ac713fc9f9ca232fd0
Emebet <emymak93@yahoo.com>
2026-01-06 21:46:28 +0100

    Intro-to-frontend Assignments

--- courses/foundation/intro-to-frontend/HYFBay/main.js
+++ courses/foundation/intro-to-frontend/HYFBay/main.js
@@ -5,7 +5,25 @@ console.log(products);
 
 // This should create the ul and the li's with the individual products details
 function renderProducts(products) {
+  
   // your code goes here
-}
+  
+  const ul = document.querySelector("ul"); 
+  ul.innerHTML = ""; 
+
+  products.forEach(product => {
+    const li = document.createElement("li");
+
+    li.innerHTML = `
+      <h3>${product.name}</h3>
+      <p>Price: ${product.price}</p>
+      <p>Rating: ${product.rating}</p>
+    `;
+
+    ul.appendChild(li);
+  });
+
+  }
+
 
 renderProducts(products); 
